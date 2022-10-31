6682579f985840af7282053527723ac687457272
blvckcenturion <blvckcenturion@gmail.com>
2022-10-30 20:13:35 -0400

    Fixed venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/blvckcenturion/Dev/University/SystemsProject1/API/venv"
+set -gx VIRTUAL_ENV "/Users/blvckcenturion/Dev/University/SystemsProject1/BackEndFinal/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
